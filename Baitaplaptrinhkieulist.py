31a0d73eb086b24057f4d660def3523108d38af0
nhattminh160901 <nhattminh160901@gmail.com>
2020-11-12 17:27:08 +0700

    Create Baitaplaptrinhkieulist.py
    Co-Authored-By: Hoàng Minh  <72189859+hoangminh303@users.noreply.github.com>

---
+++ Baitaplaptrinhkieulist.py
@@ -0,0 +1,9 @@
+x = []
+x.append(18)
+x.append("Hue")
+y = [12, 45, -1, -0.56, "abc"]
+x = x + y
+print("x: ", x)
+print("Phần tử cuối cùng là:",x[-1], "\nPhần tử đầu tiên là:",x[0])
+print("Phần tử đầu tiên đến phần tử thứ 5:", x[0:5])
+print("Phần tử thứ 3 đến cuối cùng:", x[3:])
