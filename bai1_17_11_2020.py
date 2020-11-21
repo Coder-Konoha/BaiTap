c98ab7a4b9a1b167ce80a675c616bc6b6a7b039b
nhattminh160901 <nhattminh160901@gmail.com>
2020-11-22 00:24:21 +0700

    Update bai1_17_11_2020.py
    Co-Authored-By: Hoàng Minh  <72189859+hoangminh303@users.noreply.github.com>
    Co-Authored-By: LE VAN TAN <72194651+levantanfc11@users.noreply.github.com>

--- bai1_17_11_2020.py
+++ bai1_17_11_2020.py
@@ -1,13 +1,1 @@
-"""n = int(input("Nhập n: "))
-tong = 0
-i = 1
-while i <= n:
-    tong += i
-    i += 1
-print("Tổng các số từ 1 tới", n, "là", tong)"""
-
-n = int(input("Nhập n: "))
-tong = 0
-for i in range(1, n+1):
-    tong += i
-print("Tổng các số từ 1 tới", n, "là", tong)
+Sai ví dụ chứng minh
