8d8a8049069c0e9b1668258e387ed852f8340968
nhattminh160901 <nhattminh160901@gmail.com>
2020-11-22 21:14:45 +0700

    Create bai6-7_17_11_2020.py
    Co-Authored-By: Hoàng Minh  <72189859+hoangminh303@users.noreply.github.com>
    Co-Authored-By: LE VAN TAN <72194651+levantanfc11@users.noreply.github.com>

---
+++ bai6-7_17_11_2020.py
@@ -0,0 +1,16 @@
+import random
+n = int(input("Nhập n phần tử số thực cần đưa vào list: "))
+A = []
+for a in range(n):
+    A.append((random.uniform(-n, n)))
+gtmax = A[0]
+gtmin = A[0]
+print (A)
+for i in A[1:]:
+    if i > gtmax:
+        gtmax = i
+print("Giá trị lớn nhất là:", gtmax)
+for i in A[1:]:
+    if i < gtmin:
+        gtmin = i
+print("Giá trị nhỏ nhất là:", gtmin)
