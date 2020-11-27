8679f55a4a965fe41fbdb1eb9c63fd5b886d45c4
nhattminh160901 <nhattminh160901@gmail.com>
2020-11-27 20:26:58 +0700

    Update bai5_17_11_2020.py

--- bai5_17_11_2020.py
+++ bai5_17_11_2020.py
@@ -1,5 +1,6 @@
 import random
 import math
+#Tạo một List gồm cấc số thực (có thể tạo bằng tay hoặc ngẫu nhiên)
 n = int(input("Nhập n phần tử số thực cần đưa vào list: "))
 tl = int(input("1. Tạo list bằng tay\n2. Tạo list random\nLựa chọn: "))
 pt = 1
@@ -10,17 +11,17 @@ if tl == 1:
 if tl == 2:
     for a in range(n):
         A.append(random.uniform(-n, n))
-print("Lặp truy xuất đến từng phần tử trong List và in giá trị của từng phần tử ra màn hình")
+#Lặp truy xuất đến từng phần tử trong List và in giá trị của từng phần tử ra màn hình
 for i in A:
-    print("\tPhần tử thứ %d:" %pt, i)
+    print("Phần tử thứ %d:" %pt, i)
     pt += 1
 else:
     pt = 1
-print("Lặp truy xuất đến từng phần tử trong List và thực hiện tính logarith của từng phần tử và in giá trị đó ra màn hình")
+#Lặp truy xuất đến từng phần tử trong List và thực hiện tính logarith của từng phần tử và in giá trị đó ra màn hình
 for i in A:
     if i > 0:
-        print("\tLogarith của phần tử thứ %d:" %pt, math.log10(i))
+        print("Logarith của phần tử thứ %d:" %pt, math.log10(i))
         pt += 1
     else:
-        print("\tKhông thể tính logarith của phần tử thứ %d" %pt)
+        print("Không thể tính logarith của phần tử thứ %d" %pt)
         pt += 1
