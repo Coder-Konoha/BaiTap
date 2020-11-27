51b854b91dea68c9a5b9933d63c370282ddb0999
nhattminh160901 <nhattminh160901@gmail.com>
2020-11-27 20:25:57 +0700

    Create bai12-1_24_11_2020.py
    Co-Authored-By: Hoàng Minh  <72189859+hoangminh303@users.noreply.github.com>
    Co-Authored-By: LE VAN TAN <72194651+levantanfc11@users.noreply.github.com>

---
+++ bai12-1_24_11_2020.py
@@ -0,0 +1,11 @@
+import random
+A=[]
+B=[]
+#1 - Tạo 1 số nguyên ngẫu nhiên n trong khoảng [50, 1000] đóng vai trò là số phần tử của List
+n = random.randrange(50, 1000)
+#2 - Sinh ngẫu nhiên 1 List các số nguyên trong khoảng [-1000, 1000]
+for i in range(n):
+    A.append(random.randrange(-1000,1000))
+#3 - Sinh ngẫu nhiên 1 List các số thực trong khoảng [-1000.0, 1000.0]
+for i in range(n):
+    B.append(random.uniform(-1000.0,1000.0))
