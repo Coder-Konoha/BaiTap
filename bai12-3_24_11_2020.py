15d4c37b1d2775c57e695e1d6c005030b85202b5
nhattminh160901 <nhattminh160901@gmail.com>
2020-11-28 14:56:07 +0700

    Update bai12-3_24_11_2020.py

--- bai12-3_24_11_2020.py
+++ bai12-3_24_11_2020.py
@@ -1,8 +1,8 @@
 import random
-n = random.randrange(5,10)
+n = random.randint(50, 100)
 A = []
 for i in range(n):
     rd_str = ''
-    for a in range(5):
+    for a in range(random.randint(1, 6)):
         rd_str = rd_str + random.choice('abcdefghijklmnopqrstuvwxyz')
     A.append({'name':rd_str.capitalize(), 'age': random.randint(1, 100)})
