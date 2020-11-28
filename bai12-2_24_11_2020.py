105a8fff7b3a2ae360f76cc69c5858d947103cf9
nhattminh160901 <nhattminh160901@gmail.com>
2020-11-28 10:26:56 +0700

    Create bai12-2_24_11_2020.py
    Co-Authored-By: Hoàng Minh  <72189859+hoangminh303@users.noreply.github.com>
    Co-Authored-By: LE VAN TAN <72194651+levantanfc11@users.noreply.github.com>

---
+++ bai12-2_24_11_2020.py
@@ -0,0 +1,9 @@
+import random
+import string
+A = {}
+rd_str = ''
+for i in range(5):
+    rd_str = rd_str + random.choice('abcdefghijklmnopqrstuvwxyz')
+A['name'] = rd_str.capitalize()
+A['age'] = random.randrange(1, 100)
+print(A)
