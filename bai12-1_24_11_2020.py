b8c7a94b02947307d9285536e9a6a54423166926
nhattminh160901 <nhattminh160901@gmail.com>
2020-11-28 14:56:00 +0700

    Update bai12-2_24_11_2020.py

--- bai12-1_24_11_2020.py
+++ bai12-1_24_11_2020.py
@@ -2,10 +2,10 @@ import random
 A=[]
 B=[]
 #1 - Tạo 1 số nguyên ngẫu nhiên n trong khoảng [50, 1000] đóng vai trò là số phần tử của List
-n = random.randrange(50, 1000)
+n = random.randint(50, 1000)
 #2 - Sinh ngẫu nhiên 1 List các số nguyên trong khoảng [-1000, 1000]
 for i in range(n):
-    A.append(random.randrange(-1000,1000))
+    A.append(random.randint(-1000,1000))
 #3 - Sinh ngẫu nhiên 1 List các số thực trong khoảng [-1000.0, 1000.0]
 for i in range(n):
     B.append(random.uniform(-1000.0,1000.0))
