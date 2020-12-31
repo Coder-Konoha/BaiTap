e1ec382e8fcbbcfc3f63f780bec6c738a9f4f823
nhattminh160901 <nhattminh160901@gmail.com>
2020-12-31 13:32:48 +0700

    Update bai9_17_11_2020.py

--- bai9_17_11_2020.py
+++ bai9_17_11_2020.py
@@ -4,8 +4,8 @@ import time
 shutdown = input('Bạn có muốn tắt máy (Y/N): ')
 while True:
     if shutdown == 'n' or shutdown == 'N':
-            time.sleep(30)
-            shutdown = input('Bạn có muốn tắt máy (Y/N): ')
+        time.sleep(30)
+        shutdown = input('Bạn có muốn tắt máy (Y/N): ')
     if shutdown == 'y' or shutdown == 'Y':
         os.system('shutdown /s /t 1')
         break
