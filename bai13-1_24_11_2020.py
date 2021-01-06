8d20efbd721303225eccd7e79d4b861c0aee7a5a
nhattminh160901 <72496975+nhattminh160901@users.noreply.github.com>
2021-01-06 14:02:58 +0700

    Update bai13-1_24_11_2020.py

--- bai13-1_24_11_2020.py
+++ bai13-1_24_11_2020.py
@@ -24,6 +24,7 @@ import os
 list1 = []
 list2 = []
 for root, dirs, files in os.walk('C:\\'):   #Lập trình đọc tất cả tập tin và thư mục con trực tiếp của thư mục gốc ổ đĩa C
-    list1.append(files) #quest3: Danh sách tập tin đưa vào List1
-    list2.append(dirs) #quest3: Danh sách thư mục đưa vào List2
-print(list2, list1) #quest2: in kết quả ra màn hình
+    list1.append(files) #quest3
+    list2.append(dirs) #quest3
+    break
+print(list2, list1) #quest2
